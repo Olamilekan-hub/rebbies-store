2d218e345458e2d80421a51ddc3463d50e5f3497
Olamilekan-hub <adebukolaolamilekan123@gmail.com>
2025-08-24 22:02:05 +0100

    updating

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -8,9 +8,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  output: 'standalone',
-  trailingSlash: false,
-  generateStaticParams: false,
 };
 
 export default nextConfig;
